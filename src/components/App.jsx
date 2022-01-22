0e32e16ff91fa850bc25d38778cd9da80d6f4d3e
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2022-01-22 23:14:28 +0200

    HW2-2 step 2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,26 +5,26 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
-  addContact = name => {
+  addContact = (name, number) => {
     const contact = {
       id: nanoid(),
       name,
+      number,
     };
-    console.log(contact);
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, contact],
     }));
-    console.log(this.state.contacts);
   };
 
   handleSubmit = evt => {
     evt.preventDefault();
-    const { name } = this.state;
-    this.addContact(name);
-    this.setState({ name: '' });
+    const { name, number } = this.state;
+    this.addContact(name, number);
+    this.setState({ name: '', number: '' });
   };
 
   handleChange = ({ target }) => {
@@ -32,31 +32,50 @@ export class App extends Component {
     this.setState({ [name]: value });
   };
 
+  nameInputId = nanoid();
+  numberInputId = nanoid();
+
   render() {
-    const { name } = this.state;
+    const { name, number } = this.state;
     return (
       <>
         <h1>phonebook</h1>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="">name</label>
+          <label htmlFor={this.nameInputId}>name</label>
           <input
             type="text"
             name="name"
+            id={this.nameInputId}
             value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             onChange={this.handleChange}
           />
-          <button type="submit" disabled={!name}>
+          <label htmlFor={this.numberInputId}>number</label>
+          <input
+            type="tel"
+            name="number"
+            id={this.numberInputId}
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+          <button type="submit" disabled={!name || !number}>
             Add contact
           </button>
         </form>
         <section>
           <h2>Contacts</h2>
           <ul>
-            {this.state.contacts.map(({ id, name }) => {
-              return <li key={id}>{name}</li>;
+            {this.state.contacts.map(({ id, name, number }) => {
+              return (
+                <li key={id}>
+                  {name}: {number}
+                </li>
+              );
             })}
           </ul>
         </section>
